81d7ea7f27c540e0009cea19ab0d64acb856236e
Benoit Gantaume <benoit@agilidee.com>
2021-10-12 16:22:31 +0200

    Added stack skeleton

---
+++ js/src/stack.js
@@ -0,0 +1,17 @@
+class Stack {
+    constructor() {
+    }
+
+    push(value) {
+    }
+
+    pop() {
+        return undefined;
+    }
+
+    count() {
+        return -1;
+    }
+}
+
+module.exports = Stack;
---
+++ js/test/stack.test.js
@@ -0,0 +1,8 @@
+const { assert}  = require('chai')
+const Stack = require('../src/stack.js')
+
+describe('Stack', function() {
+    it('push should stake the provided value');
+    it('pop should provide the latest value');
+    it('count should return the stack size');
+});
