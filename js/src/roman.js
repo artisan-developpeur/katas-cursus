b23838df917732cc622742e80f66895aff9078eb
Benoit Gantaume <benoit@agilidee.com>
2021-10-19 14:05:02 +0200

    Kata result

--- js/src/roman.js
+++ js/src/roman.js
@@ -1,6 +1,19 @@
 class Roman {
     decode(romanNumber){
-        return -1;
+        let result = 0;
+        for (let i = 0; i < romanNumber.length; i++) {
+            result += this.decodeSingleCharacter(romanNumber[i]);
+        }
+        return result;
+    }
+
+    decodeSingleCharacter(romanNumber) {
+        switch (romanNumber) {
+            case "I": return 1;
+            case "V": return 5;
+            case "X": return 10;
+            default: return 0;
+        }
     }
 }
 
--- js/test/roman.test.js
+++ js/test/roman.test.js
@@ -7,5 +7,31 @@ describe('Roman', function() {
         roman = new Roman();
     });
 
-    it('should decode a roman number into integer');
+    it('should decode "CDXLVIII" into integer', function() {
+        assert.equal(roman.decode("CDXLVIII"), 448);
+    });
+
+    it('should decode "MCMXCVIII" into integer', function() {
+        assert.equal(roman.decode("MCMXCVIII"), 1998);
+    });
+
+    it('should decode "I" into integer', function() {
+        assert.equal(roman.decode("I"), 1);
+    });
+
+    it('should decode "V" into integer', function() {
+        assert.equal(roman.decode("V"), 5);
+    });
+
+    it('should decode "X" into integer', function() {
+        assert.equal(roman.decode("X"), 10);
+    });
+
+    it('should be able to sum', function() {
+        assert.equal(roman.decode("II"), 2);
+    });
+
+    it('should be able to substract', function() {
+        assert.equal(roman.decode("IV"), 4);
+    });
 });
