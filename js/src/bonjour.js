9b360ba834b0b13b2e3ece27feb7336febdb347b
Benoit Gantaume <benoit@agilidee.com>
2021-10-20 10:39:38 +0200

    Kata result

--- js/src/bonjour.js
+++ js/src/bonjour.js
@@ -1,6 +1,17 @@
 class Bonjour {
+  constructor(_clock) {
+    this.clock = _clock;
+  }
+
   greet(firstName){
-      return "";
+      const clean = firstName.trim();
+      const value = clean.charAt(0).toUpperCase() + clean.slice(1);
+
+      const hour = this.clock.getHours();
+      let salutation = "Bonjour"
+      if (hour >= 18)
+        salutation =  "Bonsoir";
+      return salutation + " " + value;
   }
 }
 
--- js/test/bonjour.test.js
+++ js/test/bonjour.test.js
@@ -1,11 +1,40 @@
 const { assert}  = require('chai')
 const Bonjour = require('../src/bonjour.js')
 
+class ClockMock {
+  constructor(_value) {
+    this.value = _value;
+  }
+  getHours() {
+    return this.value;
+  }
+}
+
 describe('Bonjour', function() {
     let bonjour;
     beforeEach(function() {
-      bonjour = new Bonjour();
+      bonjour = new Bonjour(new Date());
     });
 
-  it('should say hi');
+  it('should say hi - rule #1', function() {
+    assert.equal(bonjour.greet("Benoit"), 'Bonjour Benoit');
+  });
+
+  it('should trim firstname - rule #2', function() {
+    assert.equal(bonjour.greet("  Benoit   "), 'Bonjour Benoit');
+  });
+
+  it('should capitalize first letter', function() {
+    assert.equal(bonjour.greet("benoit"), 'Bonjour Benoit');
+  })
+
+  it("should respond 'Bonjour' if it's day time", function() {
+    bonjour = new Bonjour(new ClockMock(10));
+    assert.equal(bonjour.greet("Benoit"), 'Bonjour Benoit');
+  })
+
+  it("should respond 'Bonsoir' if it's late", function() {
+    bonjour = new Bonjour(new ClockMock(19));
+    assert.equal(bonjour.greet("Benoit"), 'Bonsoir Benoit');
+  })
 });
