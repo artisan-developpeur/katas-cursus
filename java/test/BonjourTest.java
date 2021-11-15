f76973d17bf505e56e572c797b75d2219d35644d
JBonifay <joffreybonifay83@gmail.com>
2021-11-15 15:49:14 +0100

    feat: add 'Bonjour' kata for java
    add classes Bonjour / BonjourTest starter

---
+++ java/test/BonjourTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BonjourTest {
+
+    private Bonjour bonjour;
+
+    @BeforeEach
+    void setUp() {
+        bonjour = new Bonjour();
+    }
+
+    @Test
+    void testShouldSayHi() {
+
+    }
+
+}
