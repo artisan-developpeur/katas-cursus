26892ecf6db534ce2ace539fe01494958331135a
JBonifay <joffreybonifay83@gmail.com>
2021-11-15 15:52:02 +0100

    feat: add 'Bowling' kata for java
    add classes Bowling / BowlingTest starter

---
+++ java/src/Bowling.java
@@ -0,0 +1,13 @@
+public class Bowling {
+
+    // Permet de renseigner sur le score d’un tir
+    public void roll(int pins) {
+
+    }
+
+    // Permet de récupérer le score en cours
+    public int score() {
+        return -1;
+    }
+
+}
---
+++ java/test/BowlingTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BowlingTest {
+
+    private Bowling bowling;
+
+    @BeforeEach
+    void setUp() {
+        bowling = new Bowling();
+    }
+
+    @Test
+    void testShouldImplementTheBowlingGameRules() {
+
+    }
+
+}
