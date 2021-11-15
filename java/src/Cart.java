98e2a270088f1fb44b864523d8cb11171f1e85a5
JBonifay <joffreybonifay83@gmail.com>
2021-11-15 15:57:11 +0100

    feat: add 'Potter' kata for java
    add classes Cart / CartTest starter

---
+++ java/src/Cart.java
@@ -0,0 +1,13 @@
+public class Cart {
+
+    //ajoute un livre au panier
+    public void addBook(String title) {
+
+    }
+
+    // calcule le prix du panier en centimes
+    public int getPrice() {
+        return -1;
+    }
+
+}
---
+++ java/test/CartTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CartTest {
+
+    private Cart cart;
+
+    @BeforeEach
+    void setUp() {
+        cart = new Cart();
+    }
+
+    @Test
+    void testShouldImplementBusinessRules() {
+
+    }
+
+}
