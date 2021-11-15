76c909d21561ede9a926a3837bd27f99de4ee691
JBonifay <joffreybonifay83@gmail.com>
2021-11-15 15:54:06 +0100

    feat: add 'Diseur' kata for java
    add classes Diseur / DiseurTest starter

---
+++ java/src/Diseur.java
@@ -0,0 +1,8 @@
+public class Diseur {
+
+    // Calcule la prochaine chaine
+    public String suivant(String currentString) {
+        return "";
+    }
+
+}
---
+++ java/test/DiseurTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DiseurTest {
+
+    private Diseur diseur;
+
+    @BeforeEach
+    void setUp() {
+        diseur = new Diseur();
+    }
+
+    @Test
+    void testShouldImplementBusinessRules() {
+
+    }
+
+}
